9c29012fb3fd6ed1e7aa5286c1b8574e2219cc2f
Pablo de los Santos <pablo@camba.coop>
2018-02-23 15:19:42 -0300

    Dibujo en canvas con while y for

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -2,13 +2,17 @@ var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 var lineas = 30;
 var l = 0;
+var yi, xf;
+var colorcito = "#FAA"
 
-while(l < lineas){
-  dibujarLinea("#aaf", 0, 0, 10, 300);
+for(l=0; l < lineas; l++){
+  yi = 10 * l;
+  xf = 10 * (l +1);
+  dibujarLinea(colorcito, 0, yi, xf, 300);
   console.log("Linea " + l);
-  l = l + 1;
 }
-
+dibujarLinea(colorcito, 1, 1, 1, 300);
+dibujarLinea(colorcito, 1, 299, 299, 299);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal) {
   lienzo.beginPath();
